0d93021630414125f8c29e1eefc06994476a8619
George Bratsos <george.bratsos@bbv.gr>
2025-01-31 16:59:52 +0200

    fixed typo

--- src/app/manifest.js
+++ src/app/manifest.js
@@ -3,7 +3,7 @@ export default function manifest() {
     name: 'George Bratsos | Rally Driver - Simracer - Content Creator',
     short_name: 'George Bratsos | Rally Driver - Simracer - Content Creator',
     description:
-      'George Bratsos is a Greek simracer & rally driver. He promotes motorsport in Greece, through his online content and motorsport activities. From simracing to real rallies',
+      'George Bratsos is a Greek simracer & rally driver. He promotes motorsport in Greece, through his online content and motorsport activities. From simracing to real rallies.',
     start_url: '/',
     display: 'standalone',
     background_color: '#2159C7',
